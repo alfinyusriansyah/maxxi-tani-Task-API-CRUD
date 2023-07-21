c826c5c5499ad6fd0c23a245f2bfd1fa4de37a3b
alfinyusriansyah <alfinyus001@gmail.com>
2023-07-21 08:40:23 +0700

    cretae employee and division controller

--- app.py
+++ app.py
@@ -31,6 +31,69 @@ class Employess(db.Model):
     division = db.relationship('Divisions', backref='employees', lazy=True)
 
 
+# ------------------- CREATE EMPLOYEE (POST) -------------------------------
+@app.route('/employees', methods=['POST'])
+def create_employee():
+    with app.app_context():
+        data = request.get_json()
+        id = data['id']
+        name = data['name']
+        email = data['email']
+        phone = data['phone']
+        address = data['address']
+        division_id = data['division_id']
+
+        # Cek apakah id dan title sudah ada di database
+        existing_id = Employess.query.filter_by(id=data['id']).first()
+        existing_phone = Employess.query.filter_by(phone=data['phone']).first()
+
+        if existing_id :
+            return jsonify({'message': 'Id already exists!', 'code':400})
+        
+        if existing_phone :
+            return jsonify({'message': 'Number phone already exists!', 'code':400})
+
+        # Cek apakah ID divisi ada di database
+        existing_division = Divisions.query.get(division_id)
+        if not existing_division:
+            return jsonify({'message': 'Division not found!'}), 404
+
+
+        new_employee = Employess(id=id, name=name, email=email, phone=phone, 
+                                 address=address, division_id=division_id)
+        db.session.add(new_employee)
+        db.session.commit()
+
+        return jsonify({'message': 'Employee created successfully!', 'data' : data}), 201
+
+# ---------------------- GET ALL EMPLOYEE (GET) -------------------------------
+
+
+
+
+
+# --------------------- CREATE DIVISION (POST) -------------------------------
+@app.route('/divisions', methods=['POST'])
+def create_division():
+    with app.app_context():
+        data = request.get_json()
+        id = data['id']
+        name = data['name']
+
+        existing_id = Employess.query.filter_by(id=data['id']).first()
+        existing_name = Employess.query.filter_by(name=data['name']).first()
+
+        if existing_id :
+            return jsonify({'message': 'Id already exists!', 'code':400})
+        
+        if existing_name :
+            return jsonify({'message': 'Name already exists!', 'code':400})
+
+        new_division = Divisions(id=id,name=name)
+        db.session.add(new_division)
+        db.session.commit()
+
+        return jsonify({'message': 'Division created successfully!', 'division_id': new_division.id}), 201
 
 
 if __name__ == '__main__':
