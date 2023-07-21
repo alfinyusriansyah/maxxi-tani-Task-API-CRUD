e0ce69caf91297c726e906c700156c8d97b4e0fd
alfinyusriansyah <alfinyus001@gmail.com>
2023-07-21 08:33:00 +0700

    add comment

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 
-# Load variabel lingkungan dari file my .env
+# Load variabel lingkungan dari file my .env ini
 load_dotenv()
 
 app = Flask(__name__)
