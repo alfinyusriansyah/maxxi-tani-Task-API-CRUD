44cae4c175e1ef9ec6a461a388d71576dbd7354a
alfinyusriansyah <alfinyus001@gmail.com>
2023-07-26 22:56:37 +0700

    add update employee controller

--- employee_controller.py
+++ employee_controller.py
@@ -2,6 +2,7 @@ from flask import request, jsonify
 from app import app, db
 from model import Employees, Divisions
 
+# ------------------------ Create Employee -----------------------------------
 @app.route('/employees', methods=['POST'])
 def create_employee():
     with app.app_context():
@@ -31,6 +32,8 @@ def create_employee():
 
         return jsonify({'message': 'Employee created successfully!', 'data' : data}), 201
 
+
+# ------------------------ Get All Employee -----------------------------------
 @app.route("/employees", methods=['GET'])
 def get_all_employee ():
     employees = Employees.query.order_by(Employees.name,Employees.id ).all()
@@ -47,3 +50,36 @@ def get_all_employee ():
         }
         employee_list.append(data_employee)
     return jsonify(employee_list), 200
+
+# ------------------------ Get All Employee -----------------------------------
+@app.route("/employees/<int:id>", methods=["PUT"])
+def update_employee_byid(id):
+    with app.app_context():
+        data = request.get_json()
+        name = data['name']
+        email = data['email']
+        phone = data['phone']
+        address = data['address']
+        division_id = data['division_id']
+
+        existing_employee = Employees.query.filter_by(id=id).first()
+        print(existing_employee)
+        if not existing_employee:
+            return jsonify({'massage': 'id Employee not found!'}),404
+        
+        existing_division = Divisions.query.get(division_id)
+        print(existing_division)
+        if not existing_division:
+            return jsonify({'message' : 'id Division not found!'}), 404
+        
+            # Update data employee
+        existing_employee.name = name
+        existing_employee.email = email
+        existing_employee.phone = phone
+        existing_employee.address = address
+        existing_division.id = id
+        db.session.commit()
+
+        return jsonify({'message': 'Employee updated successfully!'}), 200
+    
+
